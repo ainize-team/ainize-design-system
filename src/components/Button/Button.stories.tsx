ecd3fbc4297a1d5eda308ccb37cb51c931604d10
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2021-10-18 18:35:09 +0900

    Update Button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { Story } from '@storybook/react';
 import { Button } from './Button';
+import { Plus } from 'react-feather';
 
 export default {
   title: 'Components/Button',
@@ -11,3 +12,17 @@ export default {
 export const Primary = () => <Button>Primary</Button>;
 
 export const Secondary = () => <Button type="secondary">Secondary</Button>;
+
+export const Outlined = () => <Button type="outlined">Outlined</Button>;
+
+export const Medium = () => <Button size="medium">Medium</Button>;
+
+export const Small = () => <Button size="small">Small</Button>;
+
+export const ButtonStartsWithIcon = () => (
+  <Button startIcon={<Plus size={20} />}>Button starts with Icon</Button>
+);
+
+export const ButtonEndsWithIcon = () => (
+  <Button endIcon={<Plus size={20} />}>Button ends with Icon</Button>
+);
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { styled } from '../../stitches.config';
 
-export const Button = styled('button', {
+const StyledButton = styled('button', {
   all: 'unset',
-  padding: '12px 20px',
+  display: 'inline-flex',
+  gap: '8px',
   color: 'white',
   borderRadius: '4px',
   cursor: 'pointer',
   fontFamily: '$roboto',
+  fontWeight: '500',
 
   variants: {
     type: {
@@ -18,12 +20,62 @@ export const Button = styled('button', {
         },
       },
       secondary: {
-        color: '#4A5057',
-        background: '#DFE2E6',
+        color: '$text2',
+        background: '$gray30-a4',
+
+        '&:hover': {
+          color: '$text1',
+          background: '$gray30-a8',
+        },
+      },
+      outlined: {
+        color: '$text2',
+        border: '1px solid $gray50',
+        '&:hover': {
+          color: '$violet150',
+          borderColor: '$violet100',
+        },
+      },
+    },
+    size: {
+      large: {
+        fontSize: '$2',
+        padding: '12px 20px',
+      },
+      medium: {
+        fontSize: '$1',
+        padding: '8px 12px',
+      },
+      small: {
+        fontSize: '$0',
+        padding: '6px 8px',
       },
     },
   },
   defaultVariants: {
     type: 'primary',
+    size: 'large',
   },
 });
+
+type StyleButtonProps = React.ComponentProps<typeof StyledButton>;
+
+export type ButtonProps = StyleButtonProps & {
+  startIcon?: React.ReactElement;
+  endIcon?: React.ReactElement;
+};
+
+export function Button({
+  startIcon,
+  endIcon,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <StyledButton {...props}>
+      {startIcon}
+      {children}
+      {endIcon}
+    </StyledButton>
+  );
+}
