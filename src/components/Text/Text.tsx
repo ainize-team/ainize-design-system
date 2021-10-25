fbc64f74eab98b44c36e80392482b92a79fc819a
Hanjeong Lee <hanjeong@comcom.ai>
2021-10-25 21:34:47 +0900

    Text: add lineHeight property to each text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -6,27 +6,36 @@ export const Text = styled('span', {
     size: {
       jumbo: {
         fontSize: '$6',
+        lineHeight: '64px',
       },
       h1: {
         fontSize: '$5',
+        lineHeight: '40px',
+
       },
       h2: {
         fontSize: '$4',
+        lineHeight: '32px',
       },
       h3: {
         fontSize: '$3',
+        lineHeight: '28px',
       },
       h4: {
         fontSize: '$2',
+        lineHeight: '20px',
       },
       basic: {
         fontSize: '$2',
+        lineHeight: '24px',
       },
       p: {
         fontSize: '$1',
+        lineHeight: '20px',
       },
       label: {
         fontSize: '$0',
+        lineHeight: '16px',
       },
     },
     weight: {
